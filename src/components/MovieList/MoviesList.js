f16ed048efd39f814c1a78eb8c32b2abaed5ad83
florianspk <florianspk@gmail.com>
2020-05-06 11:59:15 +0200

    add fim

--- src/components/MovieList/MoviesList.js
+++ src/components/MovieList/MoviesList.js
@@ -6,10 +6,11 @@ export default class MoviesList extends React.Component{
     render() {
         return (
             <div className="w-75 d-flex flex-row flex-wrap align-content-start">
-               <Movie movie={this.props.movies[0]} updateSelectedMovie={this.props.updateSelectedMovie}/>
+                <Movie movie={this.props.movies[0]} updateSelectedMovie={this.props.updateSelectedMovie}/>
                 <Movie movie={this.props.movies[1]} updateSelectedMovie={this.props.updateSelectedMovie}/>
                 <Movie movie={this.props.movies[2]} updateSelectedMovie={this.props.updateSelectedMovie}/>
                 <Movie movie={this.props.movies[3]} updateSelectedMovie={this.props.updateSelectedMovie}/>
+                <Movie movie={this.props.movies[4]} updateSelectedMovie={this.props.updateSelectedMovie}/>
             </div>
 
 
